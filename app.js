6b689ee1c9852ae95d78724b4d226dfd80f49638
Renaud <renaudfradin@gmail.com>
2021-01-05 21:44:08 +0100

    add findone

--- app.js
+++ app.js
@@ -32,10 +32,16 @@ app.post('/api/stuff', (req, res, next) => {
    });
 
 
-app.use('/api/stuff', (req, res, next) => {
+app.get('/api/perso', (req, res, next) => {
     modelPersonage.find()
-        .then( things =>res.status(200).json({things}))
+        .then( personagess =>res.status(200).json({personagess}))
         .catch(error=>res.status(400).json({error}));
+});
+
+app.get('/api/perso/:id', (req, res, next) => {
+    modelPersonage.findOne({ _id: req.params.id })
+        .then( personagess =>res.status(200).json({personagess}))
+        .catch(error=>res.status(400).json({ error }));
 });
 
 
