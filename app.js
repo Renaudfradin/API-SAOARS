a872a83098cfd795f03248953c037b4577ce17d2
Renaudfradin <renaudfradin@gmail.com>
2021-01-12 18:22:08 +0100

    update mogo to postgresql

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
 const modelPersonage = require("./models/Personages.js");
-const Personages = require("./models/Personages.js");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://RenaudFRADIN:Tigrette1792365020172020@cluster0.ookc7.mongodb.net/test?retryWrites=true&w=majority',
+/*mongoose.connect('mongodb+srv://RenaudFRADIN:Tigrette1792365020172020@cluster0.ookc7.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+*/
+
+const knex = require("knex")({
+    client :"pg",
+    connection:{
+        connectionString: '###',
+        ssl:{
+            rejectUnauthorized: false,
+        }
+    },
+});
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,6 +33,8 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+
+/*
 app.post('/api/stuff', (req, res, next) => {
     delete req.body._id;
     const perso = new modelPersonage({
@@ -31,7 +44,6 @@ app.post('/api/stuff', (req, res, next) => {
        .then(()=>res.status(201).json({message:"objt creee !!!!!!!"}))
        .catch(error=>res.status(400).json({error}))
    });
-
 
 app.get('/api/perso', (req, res, next) => {
     modelPersonage.find()
@@ -66,6 +78,55 @@ app.get('/api/perso/:id', (req, res, next) => {
           ]
          }));
 });
+*/
+app.get('/api/perso', async (req, res, next) => {
+
+    let persos = [];
+    try {
+      persos = await knex.select(['id','name','description','description1','attack1','descriptionattack1','mpattack1','attack2','descriptionattack2','mpattack2','attack3','descriptionattack3','mpattack3','hp','mp','atk','def','mdef','crit','spd']).from('personage');
+    } catch (error) {
+        console.log('An error occured: ', error);
+        return res.status(400).json({
+            statusCode: 400,
+            message: 'Bad Request',
+            errors:[{
+                message:'failed to query database',
+            }],
+        });
+    }
+    //console.log(users);
+    return res.status(200).json({
+        statusCode: 200,
+        message: 'succcesful / OK',
+        Personage: persos,
+    });
+});
+
+app.get('/api/perso/:id' , async (req,res,next)=>{
+  let perso = [];
+  try {
+    perso = await knex('personage').where({
+      id: req.params.id
+    })
+  } catch (error) {
+    console.log('An error occured: ', error);
+        return res.status(400).json({
+            statusCode: 400,
+            message: 'Bad Request',
+            errors:[{
+              message:'failed to query database',
+            }],
+        });
+  }
+  console.log(perso);
+  return res.status(200).json({
+    statusCode: 200,
+    message: 'succcesful / OK',
+    Personage: perso,
+});
+
+});
+
 
 
 module.exports = app;
