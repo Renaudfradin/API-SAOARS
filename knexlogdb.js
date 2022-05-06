6cf3ca0bfd29eb79155b4ef50aee8c3ba28c4725
Renaudfradin <renaudfradin@gmail.com>
2022-05-06 17:33:16 +0200

    add .env

--- knexlogdb.js
+++ knexlogdb.js
@@ -1,11 +1,15 @@
+// export const databaseurl = process.env['DATABASE_URL'];
 const knex = require("knex")({
     client : "pg",
     connection:{
-        connectionString: 'postgres://peohlgeivrzwcy:68848fb1718e4c0f24417a10d755ee629f11accba8d24d311889e34fdc3c7647@ec2-54-78-127-245.eu-west-1.compute.amazonaws.com:5432/d4g9hrsgfsus2',
+        connectionString: process.env.DATABASE_URL,
         ssl:{
             rejectUnauthorized: false,
         }
     },
 });
 
+console.log(process.env);
+console.log(process.env.USERTEST);
+console.log(process.env.DATABASE_URL);
 module.exports = knex;
