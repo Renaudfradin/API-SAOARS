c4276e698a17e440a0c291e50832116961b4253e
Renaud <renaudfradin@gmail.com>
2022-10-25 10:11:26 +0200

    remove : remove corection css vercel

--- app.js
+++ app.js
@@ -13,10 +13,9 @@ const routerBanner = require('./routes/banner.js');
 const routerImagination = require('./routes/imagination.js');
 //const routerusers = require('./routes/users.js');
 
+//options swagger ui
 var options = {
   explorer: true,
-  // customCssUrl: '/public/css/swagger-ui.css', 
-  // customSiteTitle: "The Words That I Know API - Swagger"
 };
 
 app.use((req, res, next) => {
