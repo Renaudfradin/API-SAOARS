7db0e938574a780693dec00909463382281b6e8c
Renaud <renaudfradin@gmail.com>
2024-07-15 17:11:20 +0200

    pass express to adonis.js

--- routes/banner.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require('../controllers/controllerbaner.js');
-
-router.get('/', controller.getBanner);
-
-router.get('/:idb', controller.getBannerId);
-
-module.exports = router;
